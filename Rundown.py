87f27a6cf7ce6f7f3f5bf0cc0d7bcc1c39f0edf2
0xHamilton <hamiltonalexander51@gmail.com>
2024-10-02 13:44:36 +0100

    in progress

---
+++ Rundown.py
@@ -0,0 +1,6 @@
+username=input("input the items?")
+
+if username>12:
+    print(f"your username cant be more than 12 character")
+else:
+    print(f"welcome, {username}")
--- compare.py
+++ compare.py
@@ -1,7 +1,12 @@
-x=int(input("what is x ?"))
-y=int(input("what is y?"))
+import time
 
-if x==y:
-    print("x is equal to y")
-else:
-    print("x is not equal to y")
+my_time=int(input("enter the amount "))
+
+for x in range(my_time,0,-1): 
+    seconds=x % 60
+    minutes=int(x/60)% 60
+    hours=int(x/3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("sleep tight")
